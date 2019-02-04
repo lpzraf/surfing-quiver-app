4e908180ccaea331063704acf35358b966ed143f
lpzraf <lpz.raf@gmail.com>
2019-02-04 18:35:47 -0500

    It will add the core files and folders

---
+++ Rakefile
@@ -0,0 +1,6 @@
+require_relative './config/environment'
+require 'sinatra/activerecord/rake'
+desc 'A console'
+task :console do
+Pry.start
+end
--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -1,1 +1,13 @@
-<p>ssss</p>
+require "./config/environment"
+class ApplicationController < Sinatra::Base
+configure do
+set :public_folder, "public"
+set :views, "app/views"
+enable :sessions
+set :session_secret, "surfing-quiver-app_secret"
+end
+get '/' do
+session[:greeting] = "Hello World!"
+"Hello World!"
+end
+end
