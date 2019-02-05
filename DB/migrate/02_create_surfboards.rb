ba3f4a1d9bc24778d2aac88ddf23d432fd5a2213
lpzraf <lpz.raf@gmail.com>
2019-02-04 19:36:47 -0500

    It will add the DB

---
+++ DB/migrate/02_create_surfboards.rb
@@ -0,0 +1,14 @@
+class CreateSurfboards < ActiveRecord::Migration
+
+  def change
+    create_table :surfboards do |t|
+      t.string :brand_name
+      t.string :shaper
+      t.string :length
+      t.string :width
+      t.string :tickness
+      t.string :tail
+      t.string :type_of_surf
+      t.timestamps null: false
+  end
+end
