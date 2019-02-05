b347e83e6f9b5823974ee58709ee1b2c31da934e
lpzraf <lpz.raf@gmail.com>
2019-02-04 19:06:42 -0500

    It will add App Controller and Sessions Controller

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -1,13 +1,40 @@
 require "./config/environment"
+
 class ApplicationController < Sinatra::Base
-configure do
-set :public_folder, "public"
-set :views, "app/views"
-enable :sessions
-set :session_secret, "surfing-quiver-app_secret"
-end
-get '/' do
-session[:greeting] = "Hello World!"
-"Hello World!"
-end
+  configure do
+  set :public_folder, "public"
+  set :views, "app/views"
+  enable :sessions
+  set :session_secret, "surfing-quiver-app_secret"
+  end
+
+  get "/" do
+    if logged_in?
+      redirect "/surfboard-owners/#{current_user.id}"
+    else
+      erb :'sessions/welcome'
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+  	  !!current_user
+    end
+
+    def current_user
+  		@current_surfboard_owner ||= SurfboardOwner.find_by(id: session[:surfboard_owner_id])
+    end
+
+    def authorized_to_edit?(surfboard)
+      surfboard.SurfboardOwner == current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:errors] = "You must be logged in to check out this radical page"
+        redirect '/'
+      end
+    end
+  end
 end
--- app/controller/sessions_controller.rb
+++ app/controller/sessions_controller.rb
@@ -1,1 +1,40 @@
-<h1>Login Please</h1>
+class SessionsController < ApplicationController
+
+  get '/signup' do
+    erb :'sessions/signup'
+  end
+
+  post '/surfboard-owners' do
+    @surfboard_owner = SurfboardOwner.new(params)
+    if @surfboard_owner.save
+      session[:surfboard_owner_id] = @surfboard_owner.id
+
+      redirect "/surfboard-owners/#{@surfboard_owner.id}"
+    else
+      flash[:errors] = "Wipe Out! Account creation failure: #{@surfboard_owner.errors.full_messages.to_sentence}"
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+  	erb :'sessions/login'
+  end
+
+  post '/login' do
+  	@surfboard_owner = SurfboardOwner.find_by(email: params[:email])
+
+  	if @surfboard_owner && @surfboard_owner.authenticate(params[:password])
+  		session[:surfboard_owner_id] = @surfboard_owner.id
+
+  		redirect "surfboard-owners/#{@surfboard_owner.id}"
+  	else
+      flash[:errors] = "Something's wrong. Your email or password is incorrect. Try again or Sign Up to continue"
+      redirect "/login"
+  	end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+end
