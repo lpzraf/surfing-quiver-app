5aa32f4be3941629669864219618521e69cbc0e3
lpzraf <lpz.raf@gmail.com>
2019-02-04 19:14:17 -0500

    It will add Surfboard Owners controller

---
+++ app/controller/surfboard_owners.rb
@@ -0,0 +1,16 @@
+class SurfboardOwners < ApplicationController
+
+  get "/surfboard-owners" do
+      @surfboard_owners = SurfboardOwner.all
+
+      erb :"surfboard-owners/index"
+  end
+
+  get "/surfboard-owners/:id" do
+    redirect_if_not_logged_in
+    @surfboard_owner = SurfboardOwner.find_by(id: params[:id])
+
+    erb :"surfboard-owners/show"
+  end
+
+end
